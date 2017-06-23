f803797d6e390d677ba13730b8b821ac3372e536
Olexandr Nikolaevsky <kanary@inbox.ru>
2017-06-23 20:57:31 +0300

    Learning binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,16 +2,27 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  template: `
-    <app-server></app-server>
-    <app-server></app-server>`,
-  
+  templateUrl: './servers.component.html'
 })
 export class ServersComponent implements OnInit {
-
-  constructor() { }
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = '';
+  
+  constructor() { 
+    setTimeout (() => {this.allowNewServer = true}, 2000);
+  }
 
   ngOnInit() {
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server ' + this.serverName + ' was created!';
+  }
+
+  onUpdateServerName(e) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+    console.log(this.serverName);
   }
 
 }
