d6bd59444b8e36a1c1460e9cff51375cacd596a8
Olexandr Nikolaevsky <kanary@inbox.ru>
2017-06-24 01:13:01 +0300

    Using ngFor directive

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,6 +8,8 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
   serverName = '';
+  serverCreated = false;
+  servers = ['Testserver', 'Testserver 2'];
   
   constructor() { 
     setTimeout (() => {this.allowNewServer = true}, 2000);
@@ -18,6 +20,8 @@ export class ServersComponent implements OnInit {
 
   onCreateServer() {
     this.serverCreationStatus = 'Server ' + this.serverName + ' was created!';
+    this.servers.push(this.serverName);
+    this.serverCreated = true;
   }
 
   onUpdateServerName(e) {
